5168ccebd8e6b8badd124bf1da770a9cd1c5c7af
“ARustamA” <rakhmetyanov@gmail.com>
2023-05-23 12:49:00 +0600

    pagination

---
+++ usePagination.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect, useRef, useMemo } from "react";
+
+export const usePagination = <T>(data: T[], displayPerPage: number) => {
+   const [page, setPage] = useState(0);
+
+   const isFirst = useRef(true);
+
+   useEffect(() => {
+      if (isFirst.current) {
+         isFirst.current = false;
+         return;
+      }
+      setPage(0);
+   }, [data]);
+
+   const handlePageChange = (page: number) => {
+      setPage(page);
+   };
+
+   const items = useMemo(() => {
+      return data.slice(page * displayPerPage, (page + 1) * displayPerPage);
+   }, [displayPerPage, data, page]);
+
+   return {
+      page,
+      items,
+      pageCount: Math.ceil(data.length / displayPerPage),
+      handlePageChange,
+   };
+};
